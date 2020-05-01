8e1352eabaf42ac496b72bb5aa3f18c70e97f870
Guido Mantilla <guido.mantilla@yahoo.com>
2020-05-01 01:23:38 -0500

    First Commit

--- src/main/java/valid/movies/api/ws/FilmsRepositoryWs.java
+++ src/main/java/valid/movies/api/ws/FilmsRepositoryWs.java
@@ -1,9 +1,7 @@
 package valid.movies.api.ws;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 import valid.movies.api.model.Films;
